de993d15930fdfb193e70e0d9436d6bf900b7583
Tanat <ttangun1@gmail.com>
2022-04-03 19:58:07 +0700

    chore: update strapi version to 4.1.7

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,6 @@ module.exports = {
       script: "./server.js",
       instance: "max",
       exec_moode: "cluster",
-      env: {
-        NODE_ENV: "development",
-      },
       env_production: {
         NODE_ENV: "production",
       },
